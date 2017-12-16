9f2a52ff27e9b0a6960e58d0dc9dd7a4b30cfcc6
Mike Rose <drwndx@gmail.com>
2017-12-16 12:32:53 -0700

    STARTING ALL THE THINGS

--- wtfdaSkill/src/index.js
+++ wtfdaSkill/src/index.js
@@ -126,5 +126,187 @@ const startSessionHandlers = Alexa.CreateStateHandler(states.start, {
     }
 });
 
+const practiceHandlers = Alexa.CreateStateHandler(states.PRACTICE, {
+
+    'NewSession': function() {
+        this.emit('NewSession')
+    },
+
+    'AMAZON.YesIntent': function() {
+        let say = '';
+
+        this.attributes['currentQuestionIndex'] = 0;
+
+        if (this.attributes['wrongList'].length > 0) {
+            this.attributes['sessionQuestionList'] = randomizeArray(this.attributes['wrongList']);
+            this.attributes['wrongList'] = [];
+            this.attributes['wrongCount'] = 0;
+            this.attributes['correctCount'] = 0;
+        } else {
+            this.attributes['sessionQuestionList'] = randomizeArray(this.attributes['questionList']);
+        }
+
+        say = 'First question of ' + this.attributes['sessionQuestionList'].length + ', ';
+        say += '' + this.attributes['sessionQuestionList'] [0].question + '?';
+        this.response.speak(say).listen(say);
+        this.emit(':responseReady');
+    },
+
+    'AnswerIntent' : function() {
+        var theAnswer = '';
+
+        if (this.event.request.intent.slots.answer || this.event.request.intent.slots.answer.value == '') {
+            this.emitWithState('AMAZON.HelpIntent');
+        } else {
+            theAnswer = this.event.request.intent.slots.answer.value;
+
+            this.emit('rateAnswer', theAnswer, (say) => {
+                var currentQuestionIndex = this.attributes['currentQuestionIndex'];
+
+                if (currentQuestionIndex < this.attributes['sessionQuestionList'].length) {
+                    say = say + 'Next question, ' + this.attributes[sessionQuestionList][currentQuestionIndex].quesiton + '? ';
+                    this.reponse.speak(say).listen(say);
+                    this.emit(':responseReady');
+                } else {
+                    this.handler.state = states.RECAP_PRACTICE;
+                    this.emitWithWState('ReacapSession', say);
+                }
+            });
+        }
+    },
+
+    'AMAZON.StopIntent': function () {
+        this.response.speak('Goodbye');
+        this.emit(':responseReady');
+    },
+
+    'AMAZON.HelpIntent': function () {  // practice help
+        let helpText = 'please answer the question as you would normally';
+        this.response.speak(helpText);
+        this.emit(':responseReady');
+    },
+
+    'Unhandled': function() {  // if we get any intents other than the above
+        this.response.speak('Sorry, I didn\'t get that.').listen('Try again');
+        this.emit(':responseReady');
+    }
+});
+
+const recapPracticeHandlers = Alexa.CreateStateHandler(states.RECAP_PRACTICE, {
+    'NewSession': function() {
+        this.emit('NewSession');
+    },
+
+    'RecapSession': function (say) {
+        say = say + ' Bout completed. You got ' + this.attributes['correctCount'] + ' right out of ' + this.attributes['sessionQuestionList'].length + '. ';
+
+        if (this.attributes['wrongCount'] === 0) {
+            say += ' Great job!  You can say stop if you are done. Would you like to try the Quiz now? ';
+            this.response.speak(say).listen(say);
+            this.emit(':responseReady');
+
+        } else {
+            say += ' I have sent the ' + pluralize('question', this.attributes[wrongCount]) + 'you got wrong to your Alexa app.';
+            say += ' Would you like to practice this list again now? ';
 
 
+            let cardText = '';
+            let wrongList = this.attributes['wrongList'];
+            for (let i = 0; i < wrongList.length; i++) {
+                cardText += '\n\nQuestion : ' + wrongList[i].question;
+                cardText += '\nAnswer   : ' + wrongList[i].answer[0];  // show the first acceptable answer
+            }
+
+            this.response.cardRenderer('Flash Cards to Practice', cardText);
+            this.response.speak(say).listen('You can say yes to practice, or say no to quit.');
+            this.emit(':responseReady');
+        }
+    },
+
+    'AMAZON.YesIntent': function () {
+        if (this.attributes['wrongCount'] === 0) {
+            this.handler.state = states.QUIZ;
+            this.emitWithState('AMAZON.YesIntent');
+
+        } else {
+            this.handler.state = states.PRACTICE;
+            this.emitWithState('AMAZON.YesIntent');
+        }
+
+    },
+
+    'AMAZON.NoIntent': function () {  //
+        let say = 'Okay, see you next time, goodbye!';
+        this.response.speak(say);
+        this.emit(':responseReady');
+    },
+
+    'Unhandled': function() {
+        this.response.speak('Sorry, I didn\'t get that. Try again.').listen('Try again.');
+        this.emit(':responseReady');
+    }
+
+});
+
+const recapQuizHandlers = Alexa.CreateStateHandler(states.RECAP_QUIZ, {
+    'RecapSession' : function (say) {
+        let scoreSummary = '';
+        scoreSummary = 'You got ' + this.attributes['correctCount'] + ' right out of ' + this.attributes['sessionQuestionList'].length + ', for a score of ' + Math.floor((100.0 *  this.attributes['correctCount'] / this.attributes['sessionQuestionList'].length)).toString() + ' % . ';
+
+        say = say + 'You are done.' + scoreSummary.replace('\n','') + ' I have sent this result to your Alexa App. ';
+        if (this.attributes['WrongCount'] === 0) {
+            say += ' Great Job! You can sa stop if you are done. Would you like to start over? ';
+        } else {
+            say += ' Would you like to practice some more now?';
+        }
+
+        this.response.cardRenderer(options.TITLE +  ' Flash Cards - Quiz Result', scoreSummary);
+        this.response.speak(say, 'Say yes to practice, or say no to quit.');
+        this.emit(':responseReady');
+    },
+
+
+    'AMAZON.YesIntent': function () {
+        if (this.attributes['wrongCount'] === 0) {
+
+            this.handler.state = states.START;
+            this.emitWithState('NewSession');
+
+        } else {
+            this.handler.state = states.PRACTICE;
+            this.emitWithState('AMAZON.YesIntent');
+        }
+
+    },
+    'AMAZON.NoIntent': function () {  //
+        var say = 'Okay, see you next time, goodbye!';
+        this.response.speak(say);
+        this.emit(':responseReady');
+    },
+    'Unhandled': function() {
+        this.response.speak('Sorry, I didn\'t get that. Try again.').listen('Try again.');
+        this.emit(':responseReady');
+    }
+
+});
+
+const scoreHandlers  = {
+    'rateAnswer': function (answerGuess, callback) {
+
+        let currentQuestionIndex = this.attributes['currentQuestionIndex'];
+        let currentQuestion = this.attributes['sessionQuestionList'][currentQuestionIndex];
+        if (currentQuestion.answer.indexOf(answerGuess) >= 0 ){
+            this.attributes['correctCount'] += 1;
+
+            say = answerGuess + ' is right!'
+        } else {
+            this.attributes['wrongCount'] += 1;
+
+            let wrongList = this.attributes['wrongList'];
+            wrongList.push(currentQuestion);
+            this.attributes['wrongList'] = wrongList;
+
+            say = answerGuess + ' is wrong! The correct answer to the question'
+        }
+    }
+}
